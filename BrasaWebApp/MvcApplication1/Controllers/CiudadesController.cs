5e40936b964e028faaf945d78cabd0a544deefda
AntonioO <jesus.antonio.otero@gmail.com>
2013-07-17 20:04:11 -0600

    initial setup
    created the mvc4 app and basic views and controllers

---
+++ BrasaWebApp/MvcApplication1/Controllers/CiudadesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CiudadesController : Controller
+    {
+        //
+        // GET: /Ciudades/
+
+        public ActionResult Index(string id)
+        {
+            if(String.IsNullOrWhiteSpace(id))
+                return View("CiudadesView");
+            else
+                return View("CiudadView");
+        }
+
+    }
+}
